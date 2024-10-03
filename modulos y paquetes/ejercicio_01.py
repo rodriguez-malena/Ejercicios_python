d0ef88a6532a9b5579d6aae0a3e47189e05bfe44
rodriguez-malena <maleeroodriguez@gmail.com>
2024-10-03 08:46:28 -0300

    Ejercicio modulos y paquetes

---
+++ modulos y paquetes/ejercicio_01.py
@@ -0,0 +1,8 @@
+
+from Package_Input.Input import *
+
+get_int("Ingrese un número: ","Error, reingrese",1,15,3)
+
+get_float("Ingrese un número: ","Error, reingrese numero",3.5,15.5,3)
+
+get_string("Ingrese una cadena: ","Reingrese la cadena",4,3)
